0eb438c0f6dc0b546e95bb3ef8b6560d5bc2023f
Mohammad Jabir <mjabir94@proton.me>
2026-03-06 15:08:48 -0700

    challenge complete.

--- UniterOfAdds/Program.cs
+++ UniterOfAdds/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        dynamic a = "5";
+        dynamic b = 10;
+        Console.WriteLine(Adds.Add(a, b));
     }
+}
+
+public static class Adds
+{
+    public static dynamic Add(dynamic a, dynamic b) => a + b;
+    // public static int Add(int a, int b) => a + b;
+    // public static double Add(double a, double b) => a + b;
+    // public static string Add(string a, string b) => a + b;
+    // public static DateTime Add(DateTime a, TimeSpan b) => a + b;
 }
